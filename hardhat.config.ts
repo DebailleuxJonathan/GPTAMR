0d20f24b06786664723201268c1a3ce215eb6cb6
DebailleuxJonathan <jonathandebailleux@yahoo.fr>
2023-12-01 14:19:23 +0100

    add crowdsale contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+  networks: {
+    mumbai: {
+      chainId: 80001,
+      url: `https://polygon-testnet.public.blastapi.io/0xD1C1dC47F3973dC1924E91081509aa32F69c9CaE`,
+      accounts: {
+        mnemonic: "delay daring people engage goat ghost because elder worry cluster asset ball",
+      }
+    }
+  }
 };
 
 export default config;
