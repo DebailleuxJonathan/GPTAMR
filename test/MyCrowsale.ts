cbeb4a21e880d83feed666a1486c6a2d27fb3ff5
Ldq <brian.lecarpentier@edu.itescia.fr>
2023-12-04 16:25:13 +0100

    Try add all tests

--- test/MyCrowsale.ts
+++ test/MyCrowsale.ts
@@ -1,31 +1,110 @@
-import {ethers} from "hardhat";
-import {expect} from "chai";
-
+import { ethers } from "hardhat";
+import { expect } from "chai";
 
 describe("MyCrowdsale", function () {
-    let myCrowdsale, owner, addr1, token;
+    let myCrowdsale: any;
+    let owner: any;
+    let addr1: any;
+    let addr2: any;
+    let token: any;
 
     beforeEach(async function () {
-        // Setup accounts
-        [owner, addr1] = await ethers.getSigners();
+        [owner, addr1, addr2] = await ethers.getSigners();
 
-        // Deploy the token contract first
         const Token = await ethers.getContractFactory("MyToken");
         token = await Token.deploy();
 
-        // Deploy MyCrowdsale contract
-        const MyCrowdsale = await ethers.getContractFactory("MyCrowdsale");
-        myCrowdsale = await MyCrowdsale.deploy(1, token.address, owner.address);
+        const MyCrowdsaleFactory = await ethers.getContractFactory("MyCrowdsale");
+
+        myCrowdsale = await MyCrowdsaleFactory.deploy(1, token.target, owner.address);
+
+        await token.transfer(await myCrowdsale.getAddress(), 100);
     });
 
-    it("Should set the right owner", async function () {
-        expect(await myCrowdsale.owner()).to.equal(owner.address);
+    describe("Deployment", function () {
+        it("Should set the correct owner", async function () {
+            expect(await myCrowdsale.owner()).to.equal(owner.address);
+        });
+
+        it("Should set the correct rate", async function () {
+            expect(await myCrowdsale.rate()).to.equal(1);
+        });
+
+        it("Should set the correct token address", async function () {
+            expect(await myCrowdsale.token()).to.equal(token.target);
+        });
     });
 
-    it("Should fail if rate is 0", async function () {
-        const MyCrowdsale = await ethers.getContractFactory("MyCrowdsale");
-        await expect(MyCrowdsale.deploy(0, token.address, owner.address)).to.be.rejectedWith("Rate is 0");
+    describe("buyTokens", function () {
+        it("Should buy tokens and transfer to the buyer", async function () {
+            const buyer = addr1;
+            const value = 10; // Wei, assuming rate is 1
+
+            await myCrowdsale.connect(buyer).buyTokens({ value });
+
+            const buyerBalance = await token.balanceOf(buyer)
+
+            expect(buyerBalance).to.equal(value);
+        });
+
+        it("Should fail if not enough tokens in the reserve", async function () {
+            const value = 1000000; // Wei, assuming rate is 1
+
+            await expect(myCrowdsale.connect(addr1).buyTokens({ value })).to.be.revertedWith("Not enough tokens in the reserve");
+        });
     });
 
-    // Add other constructor tests here
+    describe("withdrawTokens", function () {
+        it("Should withdraw remaining tokens to the owner", async function () {
+            const initialBalance = await token.balanceOf(owner);
+
+            expect(initialBalance).to.equal(900);
+
+            await myCrowdsale.withdrawTokens();
+
+            const finalBalance = await token.balanceOf(owner);
+
+            const remainingTokens = await token.balanceOf(await myCrowdsale.getAddress());
+
+            expect(finalBalance).to.equal(1000);
+            expect(remainingTokens).to.equal(0);
+        });
+
+        it("Should fail if no tokens to withdraw", async function () {
+            const value = 100
+
+            await myCrowdsale.connect(addr1).buyTokens({ value });
+
+            await expect(myCrowdsale.withdrawTokens()).to.be.revertedWith("No tokens to withdraw");
+        });
+    });
+
+    describe("withdrawEther", function () {
+        it("Should withdraw remaining Ether to the owner", async function () {
+            const initialBalance = await ethers.provider.getBalance(owner);
+
+            console.log('initialBalance : ', initialBalance)
+            const contractAddress = await myCrowdsale.getAddress()
+
+            // Send some Ether to the contract
+            await owner.sendTransaction({
+                to: contractAddress,
+                value: 10
+            });
+
+            console.log('sendTransaction')
+
+            await myCrowdsale.withdrawEther();
+
+            console.log('withdrawEther')
+
+            const finalBalance = await ethers.provider.getBalance(owner);
+
+            expect(finalBalance).to.equal(initialBalance);
+        });
+
+        it("Should fail if no Ether to withdraw", async function () {
+            await expect(myCrowdsale.withdrawEther()).to.be.revertedWith("No Ether to withdraw");
+        });
+    });
 });
--- test/MyToken.ts
+++ test/MyToken.ts
@@ -1,27 +1,27 @@
-// import {ethers} from "hardhat";
-// import {expect} from "chai";
-// import {Contract} from "ethers";
-//
-// describe("MyToken", function () {
-//     let MyToken: any;
-//     let token;
-//     let owner;
-//
-//     beforeEach(async function () {
-//         MyToken = await ethers.getContractFactory("MyToken");
-//         [owner] = await ethers.getSigners();
-//         token = await MyToken.deploy();
-//     });
-//
-//     describe("Déploiement", function () {
-//         it("Doit assigner l'approvisionnement initial total au propriétaire", async function () {
-//             const ownerBalance = await token.balanceOf(owner.address);
-//             expect(await token.totalSupply()).to.equal(ownerBalance);
-//         });
-//
-//         it("Doit avoir le bon nom et symbole", async function () {
-//             expect(await token.name()).to.equal("MyToken");
-//             expect(await token.symbol()).to.equal("MTK");
-//         });
-//     });
-// });
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { Contract } from "ethers";
+
+describe("MyToken", function () {
+    let MyToken: any;
+    let token;
+    let owner;
+
+    beforeEach(async function () {
+        MyToken = await ethers.getContractFactory("MyToken");
+        [owner] = await ethers.getSigners();
+        token = await MyToken.deploy();
+    });
+
+    describe("Déploiement", function () {
+        it("Doit assigner l'approvisionnement initial total au propriétaire", async function () {
+            const ownerBalance = await token.balanceOf(owner.address);
+            expect(await token.totalSupply()).to.equal(ownerBalance);
+        });
+
+        it("Doit avoir le bon nom et symbole", async function () {
+            expect(await token.name()).to.equal("MyToken");
+            expect(await token.symbol()).to.equal("MTK");
+        });
+    });
+});
